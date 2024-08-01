9acc2cb4f0ac32c1d6777d2a22788e1ce82cec03
Macarena Cherino <102886049+MacaCherino@users.noreply.github.com>
2024-08-01 14:58:36 -0300

    Add files via upload

--- aligner/eth_phy_10g_rx_aligner.v
+++ aligner/eth_phy_10g_rx_aligner.v
@@ -27,76 +27,55 @@ THE SOFTWARE.
 `timescale 1ns / 1ps
 
 /*
- * 10G Ethernet PHY dethdr
+ * 10G Ethernet PHY aligner
  */
-module eth_phy_10g_rx_aligner #
-(
-    parameter HDR_WIDTH = 2,
+module eth_phy_10g_rx_aligner
+#(
+    parameter HDR_WIDTH  = 2,
     parameter DATA_WIDTH = 64
 )
 (
-    input  wire                  clk,
-    input  wire                  rst,
+    input  wire                   clk,
+    input  wire                   rst,
 
     /*
      * SERDES interface
      */
-    input  wire [HDR_WIDTH-1:0]   serdes_rx_hdr,
-    input  wire [DATA_WIDTH-1:0]  serdes_rx_data,
+    input  wire [HDR_WIDTH-1:0 ]  i_serdes_rx_hdr ,
+    input  wire [DATA_WIDTH-1:0]  i_serdes_rx_data,
 
-    output wire [HDR_WIDTH-1:0]   serdes_rx_hdr_align,
-    output wire [DATA_WIDTH-1:0]  serdes_rx_data_align, 
-    output wire                   aligned
+    output wire [HDR_WIDTH-1:0 ]  o_serdes_rx_hdr_align ,
+    output wire [DATA_WIDTH-1:0]  o_serdes_rx_data_align, 
+    output wire                   o_aligned
 
 );
-
-// bus width assertions
-initial begin
-    if (DATA_WIDTH != 64) begin
-        $error("Error: Interface width must be 64");
-        $finish;
-    end
-    if (HDR_WIDTH != 2) begin
-        $error("Error: HDR_WIDTH must be 2");
-        $finish;
-    end
-end
 
 localparam [1:0]
     SYNC_DATA = 2'b10,
     SYNC_CTRL = 2'b01;
 
-reg [5:0]          sh_count_reg = 6'b0, sh_count_next;
-reg [3:0]          sh_invalid_count_reg = 4'b0, sh_invalid_count_next;
-reg [DATA_WIDTH:0] serdes_rx_data_reg = 64'b0, serdes_rx_data_next;
-reg [HDR_WIDTH:0]  serdes_rx_hdr_reg  = 2'b0,  serdes_rx_hdr_next;
-
-reg aligned_reg = 1'b0, aligned_next;
-
-assign serdes_rx_hdr_align = serdes_rx_hdr_reg;
-assign serdes_rx_data_align = serdes_rx_data_reg;
-assign aligned = aligned_reg;
-
-//assign rx_block_lock = rx_block_lock_reg;
+reg [5:0]            sh_count_reg        , sh_count_next        ;
+reg [3:0]            sh_invalid_count_reg, sh_invalid_count_next;
+reg [DATA_WIDTH-1:0] serdes_rx_data_reg  , serdes_rx_data_next  ;
+reg [HDR_WIDTH-1:0 ] serdes_rx_hdr_reg   , serdes_rx_hdr_next   ;
+reg                  aligned_reg         , aligned_next         ;
 
 always @* begin
-    sh_count_next = sh_count_reg;
+    sh_count_next         = sh_count_reg        ;
     sh_invalid_count_next = sh_invalid_count_reg;
 
-    serdes_rx_data_next = serdes_rx_data_reg;
-    serdes_rx_hdr_next  = serdes_rx_hdr_reg;
+    serdes_rx_data_next   = serdes_rx_data_reg  ;
+    serdes_rx_hdr_next    = serdes_rx_hdr_reg   ;
 
-    aligned_next = aligned_reg;
+    aligned_next          = aligned_reg         ;
 
-    if (serdes_rx_hdr == SYNC_CTRL || serdes_rx_hdr == SYNC_DATA) begin
+    if (i_serdes_rx_hdr == SYNC_CTRL || i_serdes_rx_hdr == SYNC_DATA) begin
         // valid header
         sh_count_next = sh_count_reg + 1;
-        //serdes_rx_data_next = serdes_rx_data;
-        //serdes_rx_hdr_next  = serdes_rx_hdr;
 
         if (&sh_count_reg) begin
             // valid count overflow, reset
-            sh_count_next = 0;
+            sh_count_next         = 0;
             sh_invalid_count_next = 0;
             if (!sh_invalid_count_reg) begin
                 aligned_next = 1'b1;
@@ -104,41 +83,44 @@ always @* begin
         end
     end else begin
         // invalid header
-        sh_count_next = sh_count_reg + 1;
+        sh_count_next         = sh_count_reg + 1        ;
         sh_invalid_count_next = sh_invalid_count_reg + 1;
-        //serdes_rx_data_next = serdes_rx_data;
-        //serdes_rx_hdr_next  = serdes_rx_hdr;
         if (!aligned_reg || &sh_invalid_count_reg) begin
             // invalid count overflow, lost alignenment
-            sh_count_next = 0;
-            sh_invalid_count_next = 0;
-            aligned_next = 1'b0;
+            sh_count_next         = 6'b0;
+            sh_invalid_count_next =  'd0;
+            aligned_next          = 1'b0;
 
             // shift one bit
-            serdes_rx_data_next = {serdes_rx_data[DATA_WIDTH-2:0],serdes_rx_hdr[HDR_WIDTH-1]};  // el ultimo bit deberia ser el primero de la siguiente trama pero para probar voy a considerar que es el primero del hdr
-            serdes_rx_hdr_next =  {serdes_rx_hdr[0],serdes_rx_data[DATA_WIDTH-1]};
+            serdes_rx_data_next = {i_serdes_rx_data[DATA_WIDTH-2:0],i_serdes_rx_hdr [HDR_WIDTH-1 ]};  // el ultimo bit deberia ser el primero de la siguiente trama pero para probar voy a considerar que es el primero del hdr
+            serdes_rx_hdr_next  = {i_serdes_rx_hdr [0]             ,i_serdes_rx_data[DATA_WIDTH-1]};
         end else if (&sh_count_reg) begin
             // valid count overflow, reset
-            sh_count_next = 0;
+            sh_count_next         = 0;
             sh_invalid_count_next = 0;
         end
     end
 end
 
 always @(posedge clk) begin
-    sh_count_reg <= sh_count_next;
-    sh_invalid_count_reg <= sh_invalid_count_next;
-    serdes_rx_hdr_reg <= serdes_rx_hdr_next;
-    serdes_rx_data_reg <= serdes_rx_data_next;
-    aligned_reg <= aligned_next;
 
     if (rst) begin
-        sh_count_reg <= 6'b0;
-        sh_invalid_count_reg <= 4'b0;
-        serdes_rx_hdr_reg <= 2'b0;
-        serdes_rx_data_reg <= 64'b0;
-        aligned_reg <= 1'b0;
+        sh_count_reg         <= 6'b0 ;
+        sh_invalid_count_reg <= 4'b0 ;
+        serdes_rx_hdr_reg    <= 2'b0 ;
+        serdes_rx_data_reg   <= 64'b0;
+        aligned_reg          <= 1'b0 ;
+    end else begin
+        sh_count_reg         <= sh_count_next        ;
+        sh_invalid_count_reg <= sh_invalid_count_next;
+        serdes_rx_hdr_reg    <= serdes_rx_hdr_next   ;
+        serdes_rx_data_reg   <= serdes_rx_data_next  ;
+        aligned_reg          <= aligned_next         ;
     end
 end
+
+assign o_serdes_rx_hdr_align  = serdes_rx_hdr_reg ;
+assign o_serdes_rx_data_align = serdes_rx_data_reg;
+assign o_aligned              = aligned_reg       ;
 
 endmodule
--- aligner/eth_phy_10g_rx_aligner_tb.v
+++ aligner/eth_phy_10g_rx_aligner_tb.v
@@ -8,8 +8,8 @@
 
 module eth_phy_10g_rx_aligner_tb;
 
-	parameter DATA_WIDTH_TB = 64;			
-	parameter HDR_WIDTH_TB = 2;			
+    parameter DATA_WIDTH_TB = 64;			
+    parameter HDR_WIDTH_TB = 2;			
 
     reg clk_tb;
     reg rx_rst_tb;
@@ -21,59 +21,58 @@ module eth_phy_10g_rx_aligner_tb;
     wire [DATA_WIDTH_TB-1:0]  serdes_rx_data_align_tb;
     wire                      aligned_tb;
 
-    integer start = 0;
-
     eth_phy_10g_rx_aligner #(
         .HDR_WIDTH(HDR_WIDTH_TB),
-    	.DATA_WIDTH(DATA_WIDTH_TB)
-	)
+        .DATA_WIDTH(DATA_WIDTH_TB)
+    )
 
-	eth_phy_10g_rx_aligner_inst (
-     	.clk(clk_tb),
-    	.rst(rx_rst_tb),
-        .serdes_rx_hdr(serdes_rx_hdr_tb),
-    	.serdes_rx_data(serdes_rx_data_tb),
-    	.serdes_rx_hdr_align(serdes_rx_hdr_align_tb),
-        .serdes_rx_data_align(serdes_rx_data_align_tb),
-    	.aligned(aligned_tb)
+    eth_phy_10g_rx_aligner_inst (
+        .clk(clk_tb),
+        .rst(rx_rst_tb),
+        .i_serdes_rx_hdr(serdes_rx_hdr_tb),
+        .i_serdes_rx_data(serdes_rx_data_tb),
+        .o_serdes_rx_hdr_align(serdes_rx_hdr_align_tb),
+        .o_serdes_rx_data_align(serdes_rx_data_align_tb),
+        .o_aligned(aligned_tb)
     );
 
     // Generación de clock
-    always begin
-    	#5 clk_tb <= ~clk_tb; 
-    end
+    always #5 clk_tb <= ~clk_tb; 
 
     initial begin
         $dumpfile("eth_phy_10g_rx_aligner_tb.vcd");
         $dumpvars(0, eth_phy_10g_rx_aligner_tb);
-   	end
+    end
 
-	always @(posedge clk_tb or posedge rx_rst_tb) begin
-		if (rx_rst_tb) begin
-        	// Resetea serdes_tx_data
-        	serdes_rx_data_tb <= 64'b0;
+    always @(posedge clk_tb or posedge rx_rst_tb) begin
+        if (rx_rst_tb) begin
+            // Resetea serdes_tx_data
+            serdes_rx_data_tb <= 64'b0;
             serdes_rx_hdr_tb  <= 2'b0;
-    	end else begin
+        end else begin
             if (serdes_rx_data_align_tb) begin
                 serdes_rx_data_tb <= serdes_rx_data_align_tb;
                 serdes_rx_hdr_tb  <= serdes_rx_hdr_align_tb;
             end
         end
 
-	end
+    end
 
 
     initial begin
         serdes_rx_data_tb = 64'b0;
-        serdes_rx_hdr_tb  = 2'b0;
-        clk_tb = 1'b0;
+        serdes_rx_hdr_tb  = 2'b0 ;
+
+        clk_tb    = 1'b0;
         rx_rst_tb = 1'b1;
+
         #500
         @(posedge clk_tb);
         rx_rst_tb = 1'b0;
         @(posedge clk_tb);
         serdes_rx_data_tb = 64'hFFFFFFFF7FFFFFFF;
         serdes_rx_hdr_tb  = 2'b11;
+        
         #2000
         $finish;
     end
